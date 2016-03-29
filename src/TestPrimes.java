eb5b515181c170f268418f1e4abf9f3e9fc6b3bb
eVonne <DingJingwen@dingjingwendeMacBook-Pro.local>
2016-03-29 19:21:58 +0800

    printPrime相关
    输出素数的程序以及其覆盖率的测试

---
+++ src/TestPrimes.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPrimes {
+
+	@Test
+	public void testPrintPrimes() {
+		printPrimes pp = new printPrimes();
+		pp.printPrimes(10);
+		
+	}
+
+}
+
